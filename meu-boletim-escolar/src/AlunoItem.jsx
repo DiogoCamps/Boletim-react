6172d882c658eb1d0a6bdd101403886210cee844
DiogoCamps <diogoapferreiracampos@gmail.com>
2025-09-09 15:24:55 -0300

    att

---
+++ meu-boletim-escolar/src/AlunoItem.jsx
@@ -0,0 +1,19 @@
+// src/components/AlunoItem.jsx
+import React from 'react';
+
+const AlunoItem = ({ aluno, onRemover, onEditar }) => {
+    const media = (aluno.n1 + aluno.n2 + aluno.n3) / 3;
+    const situacao = media >= 7 ? 'Aprovado' : 'Reprovado';
+
+    return (
+        <li>
+            <span>**Nome:** {aluno.nome}</span>
+            <span>**Média:** {media.toFixed(2)}</span>
+            <span>**Situação:** {situacao}</span>
+            <button onClick={() => onEditar(aluno)}>Editar</button>
+            <button onClick={() => onRemover(aluno.id)}>Remover</button>
+        </li>
+    );
+};
+
+export default AlunoItem;
